0e75069af926509be59fb6eccf0ae68d51aba207
AaronBrownDev <aaronb954@gmail.com>
2024-04-03 09:11:27 -0500

    update main and fix outliers

--- src/csc145exam3/Main.java
+++ src/csc145exam3/Main.java
@@ -1,5 +1,11 @@
 package csc145exam3;
-
+/**
+ * File: Main.java
+ * Author: Aaron Brown
+ * Concentration: SD
+ * Date: 04/03/2024
+ * Java class description: The driver class designed to show off that the MyRectangle class works as intended.
+ */
 import java.awt.Point;
 
 public class Main {
@@ -9,9 +15,48 @@ public class Main {
    */
   public static void main(String[] args) {
     MyRectangle rect1 = new MyRectangle(0, 0, 10, 10);
-    MyRectangle rect2 = new MyRectangle(new Point(5, 5), new Point(15, 15));
-    
-    System.out.println(rect1.isSquare());
-    System.out.println(rect2.isSquare());
+    MyRectangle rect2 = new MyRectangle(new Point(5, 5), new Point(15, 10));
+    MyRectangle edgeCase = new MyRectangle(1000, 1000, 1001, 1001);
+
+    System.out.println("Rectangle 1 before scale: " + rect1);
+    rect1.scaleIt(2);
+    System.out.println("Rectangle 1 after scale: " + rect1);
+
+    System.out.println();
+
+    System.out.println("Rectangle 2: " + rect2);
+    System.out.println("Rectangle 2 perimeter: " + rect2.getPerimeter());
+    System.out.println("Rectangle 2 area: " + rect2.getArea());
+
+    System.out.println();
+    System.out.println("Rectangle 1 area: " + rect1.getArea());
+
+    System.out.println("The combined area of Rectangle 1 and 2: " + rect1.getCombinedArea(rect2));
+    System.out.println("The combined area of Rectangle 1 and separate Rectangle: " + rect1.getCombinedArea(edgeCase));
+
+    System.out.println();
+
+    System.out.println("The intersect area of Rectangle 1 and 2: " + rect1.getIntersectArea(rect2));
+    System.out.println("The intersect area of Rectangle 1 and separate Rectangle: " + rect1.getIntersectArea(edgeCase));
+
+    System.out.println();
+
+    System.out.println("The percent of area intersected of Rectangle 1 and 2: " + rect1.getPercentIntersect(rect2));
+    System.out.println("The percent of area intersected of Rectangle 1 and separate Rectangle: " + rect1.getPercentIntersect(edgeCase));
+
+    System.out.println();
+
+    System.out.println("Rectangle 1 is touching Rectangle 2: " + rect1.touching(rect2));
+    System.out.println("Rectangle 1 is touching Separate Rectangle: " + rect1.touching(edgeCase));
+
+    System.out.println();
+
+    System.out.println("Rectangle 1 is landscape shaped: " + rect1.isLandscape());
+    System.out.println("Rectangle 2 is landscape shaped: " + rect2.isLandscape());
+
+    System.out.println();
+
+    System.out.println("Rectangle 1 is a square: " + rect1.isSquare());
+    System.out.println("Rectangle 2 is a square: " + rect2.isSquare());
   }
 }
--- src/csc145exam3/MyRectangle.java
+++ src/csc145exam3/MyRectangle.java
@@ -1,16 +1,17 @@
 package csc145exam3;
-
+/**
+ * File: MyRectangle.java
+ * Author: Aaron Brown
+ * Concentration: SD
+ * Date: 04/03/2024
+ * Java class description: Creates a custom rectangle class as a java.awt.rectangle as a base and adds more methods to it
+ */
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.lang.Math;
 /*
   * Inheriting from the java.awt.Rectangle class
  * See: https://docs.oracle.com/javase/8/docs/api/java/awt/Rectangle.html
- */
-
-/**
- *
- * @author Aaron
  */
 public class MyRectangle extends Rectangle {
   public int x2;  // x + width or p2.x or
@@ -72,12 +73,16 @@ public class MyRectangle extends Rectangle {
   // --- Add the following methods, along with JavaDoc. Use existing methods from Rectangle when implementing the
   // --- methods below.
   // --- public int getPerimeter()
-  public int getPerimiter(){
+  public int getPerimeter(){
+      this.width = (int)Math.abs(this.x - this.x2);
+      this.height = (int)Math.abs(this.y - this.y2);
       return this.width * 2 + this.height * 2;
   }
   
   // --- public int getArea()
   public int getArea(){
+      this.width = (int)Math.abs(this.x - this.x2);
+      this.height = (int)Math.abs(this.y - this.y2);
       return this.width * this.height;
   }
   
@@ -100,8 +105,13 @@ public class MyRectangle extends Rectangle {
   // ---   object of type MyRectangle)
   // --- public int getIntersectArea(Rectangle rect)
   public int getIntersectArea(Rectangle rect){
-      Rectangle intersectRect = this.intersection(rect);
-      return intersectRect.width * intersectRect.height;
+      if (this.intersects(rect)) {
+          Rectangle intersectRect = this.intersection(rect);
+          return intersectRect.width * intersectRect.height;
+      } else {
+          return -1;
+      }
+
   }
   // --- public int getIntersectArea(MyRectangle rect)
   public int getIntersectArea(MyRectangle rect){
@@ -111,7 +121,11 @@ public class MyRectangle extends Rectangle {
   // ---   are not overlapping or touching, return -1. See the note about not duplicating the code above.
   // --- public float getPercentIntersect(Rectangle rect)
   public float getPercentIntersect(Rectangle rect){
-      return (float)this.getIntersectArea(rect) / this.getCombinedArea(rect) + this.getIntersectArea(rect);
+      if (this.intersects(rect)) {
+          return (float)this.getIntersectArea(rect) / (this.getCombinedArea(rect) + this.getIntersectArea(rect));
+      } else {
+          return 0;
+      }
   }
   // --- public float getPercentIntersect(MyRectangle rect)
   public float getPercentIntersect(MyRectangle rect){
@@ -131,12 +145,16 @@ public class MyRectangle extends Rectangle {
   // ---   are touching, return true, false otherwise. See the note about not duplicating the code above.
   // --- public boolean isLandscape()
   public boolean isLandscape(){
+      this.width = (int)Math.abs(this.x - this.x2);
+      this.height = (int)Math.abs(this.y - this.y2);
       return this.width > this.height;
   }
   // ---   Returns true if the rectangle is wider than taller, false otherwise.
   
   // --- public boolean isSquare()
     public boolean isSquare(){
+        this.width = (int)Math.abs(this.x - this.x2);
+        this.height = (int)Math.abs(this.y - this.y2);
         return this.width == this.height;
   }
   // ---   Returns true if the width and height of the rectangle are the same, false otherwise.
